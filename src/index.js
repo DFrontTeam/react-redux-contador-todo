bc8a1d23005b18abd2dda55a92aee59d242892fb
DFrontTeam <122452114+DFrontTeam@users.noreply.github.com>
2023-03-01 10:19:49 +0100

    add thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import App from './App';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { reducers } from './store';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-import App from './App';
-
-const store = createStore(reducers);
+const store = createStore(reducers,applyMiddleware(thunk));
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
