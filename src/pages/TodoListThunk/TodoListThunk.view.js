b9a5724de25951b41c24a463911d62cad5c8b758
DFrontTeam <122452114+DFrontTeam@users.noreply.github.com>
2023-03-02 11:42:28 +0100

    1 file added

---
+++ src/pages/TodoListThunk/TodoListThunk.view.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+const TodoListThunk = () => {
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Consíguelos a todos</h1>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'start',
+          width: '100%',
+        }}
+      >
+        <div>
+          <h3>Pokemon por capturar</h3>
+          <ul>
+
+          </ul>
+        </div>
+        <div>
+          <h3>Pokemon capturados</h3>
+          <ul>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoListThunk;
